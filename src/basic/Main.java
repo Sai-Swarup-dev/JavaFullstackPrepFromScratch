2d85bf3872b1986ef5802eeb0a74871c48ef11d3
Sai-Swarup-dev <saiswaruppatnaik14@gmail.com>
2024-10-19 09:42:01 +0530

    Main Function

---
+++ src/basic/Main.java
@@ -0,0 +1,15 @@
+package basic;
+
+class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World...");
+    }
+}
+
+//Note:
+/*
+ * Public: it is a access modifier
+ * static: we can call it using class name or not need of object creation
+ * void: null/no return value
+ * main: function
+ */
